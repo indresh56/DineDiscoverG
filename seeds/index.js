ec53922113f25193e1e9ddafb2bf4b291eb92a0b
indresh56 <85017596+indresh56@users.noreply.github.com>
2024-06-19 21:02:19 +0530

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const pet = require('../models/restaurant');
+const restaurant = require('../models/restaurant');
 mongoose.connect('mongodb://localhost:27017/dine-discover', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -11,10 +11,10 @@ db.once("open", () => {
 })
 
 const seedfun = async () => {
-    await pet.deleteMany({});
+    await restaurant.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const temp = new pet({
-            title: `Pet ${i}`,
+        const temp = new restaurant({
+            title: `Restaurant ${i}`,
             image: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
             description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, dicta vel, dolor ea velit beatae facere dolorum, ullam repellendus aut quasi provident non ${i}`,
             location: 'Hyderabad, Telangana',
